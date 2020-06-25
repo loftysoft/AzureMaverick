59fc013003bdd089cb88d44c5aeaf1d3458857d2
Magnus Mårtensson <magnus@loftysoft.com>
2020-06-25 09:13:13 +0200

    updating links

---
+++ Demo.Application/Instrumented/Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Loftysoft.Demo.Web
+{
+	public static class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+			filters.Add(new AiHandleErrorAttribute());
+		}
+	}
+}
---
+++ Demo.Application/Instrumented/Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Loftysoft Demo Web App.")]
+[assembly: AssemblyDescription("A demo application to show Azure Web App and Application Insight features.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Loftysoft")]
+[assembly: AssemblyProduct("Demo Web App")]
+[assembly: AssemblyCopyright("Copyright © Loftysoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dc091c8d-49b5-4b4e-816f-1ff21fdf8252")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
