7849aa1b67bbabb3577b8eb36a1b46ef5e92570e
Magnus Mårtensson <magnus@loftysoft.com>
2020-06-25 22:54:06 +0200

    Updating Azure Maverick demo site on GitHub.

---
+++ Demo.Application/Original/Contracts/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Loftysoft.Demo.Contracts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Loftysoft.Demo.Contracts")]
+[assembly: AssemblyCopyright("Copyright © Loftysoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("06eae574-bd9e-4d74-980e-5127ce341a24")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
