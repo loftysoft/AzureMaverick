2e3ebf372e4538eb4251ae48e88ee052306d802b
Magnus Mårtensson <magnus@loftysoft.com>
2020-06-25 18:48:21 +0200

    Clearing the repo.

--- Demo.Application/Original/Contracts/IMessageRepository.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Loftysoft.Demo.Contracts
-{
-	public interface IMessageRepository
-	{
-		Task AddMessageAsync(string name, string message, string userHostAddress);
-	}
-}
