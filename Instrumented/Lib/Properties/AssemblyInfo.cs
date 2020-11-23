425fef71c7399813e46be43fa839314fed56b4cc
Magnus Mårtensson <magnus@loftysoft.com>
2020-11-23 23:20:00 +0100

    Updating Azure Maverick demo site on GitHub.

---
+++ Instrumented/Lib/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Loftysoft.Demo.Lib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Loftysoft.Demo.Lib")]
+[assembly: AssemblyCopyright("Copyright © Loftysoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("eb2af997-9021-487c-9fef-29426bcb4344")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Instrumented/Web/ActionTimerAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace Loftysoft.Demo.Web
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+	public class ActionTimerAttribute : ActionFilterAttribute
+	{
+		const string ActiontimerPlaceholder = "_ActionTimer";
+		public const string ViewDataKey = "_ElapsedTime";
+
+		public ActionTimerAttribute()
+		{
+			Order = int.MaxValue;
+		}
+
+		public override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			var controller = filterContext.Controller;
+			if (controller != null)
+			{
+				var timer = Stopwatch.StartNew();
+				controller.ViewData[ActiontimerPlaceholder] = timer;
+			}
+			base.OnActionExecuting(filterContext);
+		}
+
+		public override void OnActionExecuted(ActionExecutedContext filterContext)
+		{
+			var controller = filterContext.Controller;
+
+			// ReSharper disable once UseNullPropagation
+			if (controller == null) return;
+
+			var timer = (Stopwatch)controller.ViewData[ActiontimerPlaceholder];
+
+			if (timer == null) return;
+
+			timer.Stop();
+			controller.ViewData[ViewDataKey] = (int?)timer.ElapsedMilliseconds;
+		}
+	}
+}
